7164b64aa26b64af6fd0c41e0a2084c9703975c1
Ashaka Egerega <ashakaegerega@gmail.com>
2022-07-04 09:40:44 +0100

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello TEST 47 World!';
+    return 'We now dispatch TEST 47 World!';
   }
 }
